547297c092143ad636617c6c5d2064fb015aa924
AbsoluteBender <uglin76@mail.ru>
2018-10-14 03:48:22 +0300

    Update src/MyFirstBotApp.js

--- src/MyFirstBotApp.js
+++ src/MyFirstBotApp.js
@@ -43,4 +43,53 @@ class MyFirstBotApp {
          * @type {string}
          */
         let messageText =
-            '
+            'Привет, ' + this._getName(msg);
+
+        for (let i = 0; i < this._botsBasedOnThisTemplate.length; i++)
+            messageText += this._botsBasedOnThisTemplate[i] + ';\n';
+
+        messageText += '\nEnjoy cooking with <a href="https://github.com/VadimCpp/myfirstbotbot">this template</a>!';
+
+        bot.sendMessage(msg.chat.id, messageText, {
+            parse_mode: "HTML",
+            disable_web_page_preview: true,
+        });
+    }
+
+    /**
+     * @param {Object} msg
+     * @return {string}
+     * @private
+     */
+    _getName(msg) {
+        /**
+         * @type {string}
+         */
+        let result = 'Ð‘ÐµÐ· Ð¸Ð¼ÐµÐ½Ð¸ ðŸ‘¤';
+
+        /**
+         * @type {string}
+         */
+        let fname = msg.from.first_name;
+
+        /**
+         * @type {string}
+         */
+        let lname = msg.from.last_name;
+
+        /**
+         * @type {string}
+         */
+        let uname = msg.from.username;
+
+        if (fname) {
+            result = fname + ( lname ? ' ' + lname : '') ;
+        } else if (uname) {
+            result = uname;
+        }
+
+        return result;
+    }
+}
+
+module.exports = MyFirstBotApp;
